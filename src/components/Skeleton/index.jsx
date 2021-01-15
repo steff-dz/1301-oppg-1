b5c3c3a8acca90ba56f1710b51e7f2a6eb54dbef
Steff <stefaniemarie@gmail.com>
2021-01-15 19:25:21 +0100

    finished the skeleton!

--- src/components/Skeleton/index.jsx
+++ src/components/Skeleton/index.jsx
@@ -25,25 +25,53 @@ const SkeletonBase = styled.article`
 		height: 150px;
 		width: 150px;
 		margin: 1rem;
-		background-color: #424141;
+		background-color: #7a7a7ad4;
+		animation-name: loadingColor;
+		animation-duration: 2s;
+		animation-iteration-count: infinite;
 	}
 
 	h2 {
 		height: 50px;
 		width: 150px;
-		background-color: #424141;
+		background-color: #7a7a7ad4;
 		margin: 3rem 1rem;
 		border-radius: 15px;
+		animation-name: loadingColor, loadingWidth;
+		animation-duration: 2s;
+		animation-iteration-count: infinite;
 	}
 
 	.skeleton-email {
 		position: absolute;
 		top: 50%;
 		left: 20%;
-		background-color: #424141;
+		background-color: #7a7a7ad4;
 		width: 250px;
 		height: 40px;
 		border-radius: 15px;
+		animation-name: loadingColor, loadingWidth;
+		animation-duration: 2s;
+		animation-iteration-count: infinite;
+	}
+
+	@keyframes loadingColor {
+		from {
+			opacity: .1;
+		}
+		to {
+			opacity: 1;
+		}
+	}
+
+	@keyframes loadingWidth {
+		from {
+			width: 150px;
+		}
+
+		to {
+			width: 200px;
+		}
 	}
 `;
 
