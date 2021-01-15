b1e399b3608c83d4503c33bbf34559d8dacf4856
Steff <stefaniemarie@gmail.com>
2021-01-15 16:39:43 +0100

    added props to component

--- src/App.js
+++ src/App.js
@@ -1,56 +1,64 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
-import GlobalStyle from './components/GlobalStyle/index'
-import PageHeader from './components/PageHeader/index'
+import GlobalStyle from './components/GlobalStyle/index';
+import PageHeader from './components/PageHeader/index';
 //import Skeleton from './components/Skeleton.index'
-import UserContainer from './components/UserContainer/index'
+import UserContainer from './components/UserContainer/index';
 
 function App() {
-  const [ responseData, setResponseData ] = useState([]);
+	const [ responseData, setResponseData ] = useState([]);
 
-  useEffect(() => {
-    console.log(responseData)
-  }, [responseData])
+	useEffect(
+		() => {
+			console.log(responseData);
+		},
+		[ responseData ]
+	);
 
-  const handleUsers = () => {
-  fetch('https://reqres.in/api/users?delay=3')
-  .then((response) => {
-    return response.json()
-  })
-  .then((data) => {
-    setResponseData(data.data);
-    console.log(responseData)
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+	const handleUsers = () => {
+		fetch('https://reqres.in/api/users?delay=3')
+			.then((response) => {
+				return response.json();
+			})
+			.then((data) => {
+				setResponseData(data.data);
+				console.log(responseData);
+			})
+			.catch((error) => {
+				console.log(error);
+			});
+	};
 
-  }
+	return (
+		<React.Fragment>
+			<GlobalStyle />
+			<PageHeader>
+				<h1>Hallo verden!</h1>
+				<button onClick={handleUsers}>Get Users</button>
+			</PageHeader>
 
-  return (
-    <>
-    <GlobalStyle/>
-    <PageHeader>
-      <h1>Hallo verden!</h1> 
-      <button onClick={handleUsers}>Get Users</button>
-    </PageHeader>
-
-      <p>
-			Stuff will go here: <b />
-      </p> 
-      <pre>
-        <code>
-          {JSON.stringify(responseData, null, 2)}
-        </code>
-      </pre>
-      <main>
-
-      {responseData.map((el) => <UserContainer key={el.id} />)}
-      </main>
-
-    </>
-  )
-};
-
+			<p>
+				Stuff will go here: <b />
+			</p>
+			<pre>
+				<code>{JSON.stringify(responseData, null, 2)}</code>
+			</pre>
+			<main>
+				{responseData.map((el) => (
+					<UserContainer key={el.id} firstName={el.first_name} lastName={el.last_name} avatar={el.avatar} />
+				))}
+			</main>
+		</React.Fragment>
+	);
+}
 
 export default App;
+
+// async function onFetchingUsers() {
+//   const response = await fetch('https://reqres.in/api/users?delay=3')
+//   const data = await response.data
+// }
+
+// onFetchingUsers().catch(error => {
+//   console.log(error)
+// })
--- src/components/UserContainer/index.jsx
+++ src/components/UserContainer/index.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
-const UserContainer = () => {
-	return <article>Hello</article>;
+const UserContainer = ({ id, firstName, lastName, avatar }) => {
+	return (
+		<article>
+			<img src={avatar} />
+			<h2>{firstName}</h2>
+			<h3>{lastName}</h3>
+		</article>
+	);
 };
 
 export default UserContainer;
