d5b401a0f7738f1ce25706f5118169e699d9b88a
Steff <stefaniemarie@gmail.com>
2021-01-15 19:09:50 +0100

    added skeleton and it's styling

--- src/App.js
+++ src/App.js
@@ -2,27 +2,46 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import GlobalStyle from './components/GlobalStyle/index';
 import PageHeader from './components/PageHeader/index';
-//import Skeleton from './components/Skeleton.index'
+import Skeleton from './components/Skeleton';
 import UserContainer from './components/UserContainer/index';
 
 function App() {
 	const [ responseData, setResponseData ] = useState([]);
+	const [ isLoaded, setIsLoaded ] = useState(false);
 
 	useEffect(() => {}, [ responseData ]);
 
 	const handleUsers = () => {
-		fetch('https://reqres.in/api/users?delay=1')
+		fetch('https://reqres.in/api/users?delay=3')
 			.then((response) => {
 				return response.json();
 			})
 			.then((data) => {
 				setResponseData(data.data);
+				setIsLoaded(true);
 				//console.log(responseData);
 			})
 			.catch((error) => {
 				console.log(error);
 			});
 	};
+
+	function renderSkeleton() {
+		console.log('loading');
+		return <Skeleton />;
+	}
+
+	function renderPage() {
+		return responseData.map((el) => (
+			<UserContainer
+				key={el.id}
+				firstName={el.first_name}
+				lastName={el.last_name}
+				avatar={el.avatar}
+				email={el.email}
+			/>
+		));
+	}
 
 	return (
 		<React.Fragment>
@@ -32,17 +51,7 @@ function App() {
 				<button onClick={handleUsers}>Get Users</button>
 			</PageHeader>
 
-			<MainBase>
-				{responseData.map((el) => (
-					<UserContainer
-						key={el.id}
-						firstName={el.first_name}
-						lastName={el.last_name}
-						avatar={el.avatar}
-						email={el.email}
-					/>
-				))}
-			</MainBase>
+			<MainBase>{isLoaded ? renderPage() : [ 1, 2, 3 ].map((n) => <Skeleton />)}</MainBase>
 		</React.Fragment>
 	);
 }
--- src/components/Skeleton/index.jsx
+++ src/components/Skeleton/index.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const Skeleton = ({ children }) => {
+const Skeleton = () => {
 	return (
-		<article>
-			<div />
+		<SkeletonBase>
+			<div className="skeleton-image" />
 			<h2 />
-		</article>
+			<h2 />
+			<div className="skeleton-email" />
+		</SkeletonBase>
 	);
 };
+
+const SkeletonBase = styled.article`
+	display: flex;
+	position: relative;
+	margin: 0 8rem;
+	background-color: #131320;
+	border-radius: 10px;
+	height: 20rem;
+	width: 60vw;
+	.skeleton-image {
+		border-radius: 50%;
+		height: 150px;
+		width: 150px;
+		margin: 1rem;
+		background-color: #424141;
+	}
+
+	h2 {
+		height: 50px;
+		width: 150px;
+		background-color: #424141;
+		margin: 3rem 1rem;
+		border-radius: 15px;
+	}
+
+	.skeleton-email {
+		position: absolute;
+		top: 50%;
+		left: 20%;
+		background-color: #424141;
+		width: 250px;
+		height: 40px;
+		border-radius: 15px;
+	}
+`;
 
 export default Skeleton;
